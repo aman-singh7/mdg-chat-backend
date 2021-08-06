5f52b71424950c302cdf64ae663023ebbb3258ca
aman-singh7 <aman2@me.iitr.ac.in>
2021-08-06 17:00:42 +0530

    feat: implement chat consumer

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -83,6 +83,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatapp.wsgi.application'
 ASGI_APPLICATION = 'chatapp.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
